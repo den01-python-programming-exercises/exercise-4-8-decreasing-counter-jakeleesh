39a6d80e5e558af6bb73147955da696234e83b45
Jake Lee <jake.lee@fdmgroup.com>
2021-10-11 21:34:01 +0800

    Attempts solution

--- src/decreasing_counter.py
+++ src/decreasing_counter.py
@@ -9,5 +9,9 @@ class DecreasingCounter:
     def decrement(self):
         # write the method implementation here
         # the aim is to decrement the value of the counter by one
+        if (self.value > 0):
+            self.value -= 1
 
     # and the other methods go here
+    def reset(self):
+        self.value = 0
